6e00ea51115f8a6e91720c90dae7499801a9741a
kivimango <dev@c0d3r.nhely.hu>
2017-05-07 12:23:27 +0200

    Changed logic

--- src/main/java/com/kivimango/jalculator/Main.java
+++ src/main/java/com/kivimango/jalculator/Main.java
@@ -27,39 +27,71 @@ public class Main extends Application {
         String[] operations = line.split("[0-9]+");
         String[] numbers = line.split("[" + Pattern.quote("+-*/") + "]");
 
-        System.out.print("Numbers: " + Arrays.toString(numbers));
-        System.out.print("Operations: " + Arrays.toString(operations));
+        System.out.println("Numbers: " + Arrays.toString(numbers));
+        System.out.println("Operations: " + Arrays.toString(operations));
 
+        int[] numbersConverted = convert(numbers);
+
+        System.out.println("Result: " + calculate(operations, numbersConverted));
+    }
+
+    private static int[] convert(String[] numbers) {
         int[] numbersConverted = new int[numbers.length];
-
         for(int i = 0; i < numbers.length; i++) {
             numbersConverted[i] = Integer.valueOf(numbers[i]);
         }
-
         System.out.print(Arrays.toString(numbersConverted));
+        return numbersConverted;
+    }
 
-        int result = numbersConverted[0];
-
-        // Use 1.7 compiler or Java 7 else it wont compile
-
-        for(int i = 1; i < operations.length; i++) {
-            switch(operations[i]) {
-                case "+" :
-                    result = result + numbersConverted[i];
-                    break;
-                case "-" :
-                    result = result - numbersConverted[i];
-                    break;
-                case "*" :
-                    result = result * numbersConverted[i];
-                    break;
-                case "/" :
-                    result = result / numbersConverted[i];
-                    break;
+    private static int calculate(String[] operations, int[] numbers) {
+        int length = operations.length;
+        int index = 1;
+        while (index < length) {
+            if ("*".equals(operations[index])) {
+                numbers[index - 1] = numbers[index - 1] * numbers[index];
+                for (int j = 1; j < length - 1; j++) {
+                    numbers[j] = numbers[j + 1];
+                    operations[j] = operations[j + 1];
+                }
+                for (int j = 1; j < length - 1; j++) {
+                    numbers[j] = numbers[j + 1];
+                    operations[j] = operations[j + 1];
+                }
+                length--;
+            } else if ("/".equals(operations[index])) {
+                numbers[index - 1] = numbers[index - 1] / numbers[index];
+                for (int j = 1; j < length - 1; j++) {
+                    numbers[j] = numbers[j + 1];
+                    operations[j] = operations[j + 1];
+                }
+                length--;
+            } else {
+                index++;
             }
         }
 
-        System.out.print("The result : " + result);
+        index = 1;
+        while (index < length) {
+            if ("+".equals(operations[index])) {
+                numbers[index - 1] = numbers[index - 1] + numbers[index];
+                for (int j = 1; j < length - 1; j++) {
+                    numbers[j] = numbers[j + 1];
+                    operations[j] = operations[j + 1];
+                }
+                length--;
+            } else if ("-".equals(operations[index])) {
+                numbers[index - 1] = numbers[index - 1] - numbers[index];
+                for (int j = 1; j < length - 1; j++) {
+                    numbers[j] = numbers[j + 1];
+                    operations[j] = operations[j + 1];
+                }
+                length--;
+            } else {
+                index++;
+            }
+        }
+        return numbers[0];
     }
 
     public static void main(String[] args) {

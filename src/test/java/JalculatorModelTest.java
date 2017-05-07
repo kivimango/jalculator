43d920018f38fa97d0b468f3796481967406adb5
kivimango <dev@c0d3r.nhely.hu>
2017-05-07 15:27:17 +0200

    Added some tests

---
+++ src/test/java/JalculatorModelTest.java
@@ -0,0 +1,47 @@
+import com.kivimango.jalculator.JalculatorModel;
+import com.kivimango.jalculator.JalculatorModelImpl;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class JalculatorModelTest {
+
+    @Test
+    public void calculateAdditionTest() {
+        JalculatorModel jalculator = new JalculatorModelImpl();
+        String input = "1+2+3";
+        int expectedResult = 6;
+        assertEquals(expectedResult, jalculator.calculate(input));
+    }
+
+    @Test
+    public void calculateSubstractionTest() {
+        JalculatorModel jalculator = new JalculatorModelImpl();
+        String input = "23-89-15";
+        int expectedResult = -81;
+        assertEquals(expectedResult, jalculator.calculate(input));
+    }
+
+    @Test
+    public void calculateMultiplicationTest() {
+        JalculatorModel jalculator = new JalculatorModelImpl();
+        String input = "1*2*3";
+        int expectedResult = 6;
+        assertEquals(expectedResult, jalculator.calculate(input));
+    }
+
+    @Test
+    public void calculateDivisonTest() {
+        JalculatorModel jalculator = new JalculatorModelImpl();
+        String input = "35/7";
+        int expectedResult = 5;
+        assertEquals(expectedResult, jalculator.calculate(input));
+    }
+
+    @Test
+    public void calculateMixedOperationsTest() {
+        JalculatorModel jalculator = new JalculatorModelImpl();
+        String input = "1+2*3";
+        int expectedResult = 7;
+        assertEquals(expectedResult, jalculator.calculate(input));
+    }
+}

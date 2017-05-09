1551eec0862201a755daf37c8561a164ee0186de
kivimango <dev@c0d3r.nhely.hu>
2017-05-09 16:17:48 +0200

    Removed unused imports and commented code

--- src/main/java/com/kivimango/jalculator/Jalculator.java
+++ src/main/java/com/kivimango/jalculator/Jalculator.java
@@ -1,21 +1,12 @@
 package com.kivimango.jalculator;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Jalculator {
+
     public static final String APP_TITLE = "Jalculator";
 
-    static JalculatorModel jalculator = new JalculatorModelImpl();
-
     public static void main(String[] args) throws IOException {
-        /**
-        System.out.print("Input your math expression: ");
-        String line = new BufferedReader(new InputStreamReader(System.in)).readLine();
-        int result = jalculator.calculate(line);
-        System.out.println("Result: " + result);
-         */
         JalculatorGui gui = new JalculatorGui();
     }
 }

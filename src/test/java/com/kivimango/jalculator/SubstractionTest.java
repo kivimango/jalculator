a4022c7cb0b556c1a7cf0cb9f3b5aa94e4b60517
kivimango <dev@c0d3r.nhely.hu>
2017-05-09 17:11:09 +0200

    Fixed expected result

--- src/test/java/com/kivimango/jalculator/SubstractionTest.java
+++ src/test/java/com/kivimango/jalculator/SubstractionTest.java
@@ -10,7 +10,7 @@ public class SubstractionTest {
         Substraction substraction = new Substraction();
         assertEquals("-", substraction.getOperator());
         assertEquals(23, substraction.calculate(56, 33));
-        //assertEquals(-42, substraction.calculate(100, -142));
+        assertEquals(242, substraction.calculate(100, -142));
         assertEquals(-13, substraction.calculate(-6, 7));
         assertEquals(9, substraction.calculate(-12, -21));
     }
